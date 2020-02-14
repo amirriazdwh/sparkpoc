2ba08f625e5e62c33e8e0cdb4af7df1445614008
amirriazdwh <61025177+amirriazdwh@users.noreply.github.com>
2020-02-14 04:21:21 +0400

    testing the code

--- src/main/scala/training/sqlFileProcessing/loadAvroFile.scala
+++ src/main/scala/training/sqlFileProcessing/loadAvroFile.scala
@@ -1,15 +1,19 @@
 package training.sqlFileProcessing
 
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object loadAvroFile {
   def main (args:Array[String]): Unit = {
+    val prop = ConfigFactory.load()
+    val envprop = prop.getConfig(args(0))
+    val inputdir = envprop.getString("dev.input.base.dir")
     val config  = new SparkConf().setAppName("Load Avro Data")
     val sc = new SparkContext(config)
     val sqlContext = new SQLContext(sc)
     sqlContext.read.format("com.databricks.spark.avro")
-                    .load("/user/hive/warehouse/userdata1.avro")
+                    .load(inputdir)
                     .registerTempTable("AVRO_DATA")
     val df = sqlContext.sql("select * from AVRO_DATA")
     df.show(10)
